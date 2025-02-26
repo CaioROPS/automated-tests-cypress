89568cbdb289b76cbd846e22dd6e2f53fb4d1cdd
Caio Ricardo <103211365+CaioROPS@users.noreply.github.com>
2025-02-26 17:17:27 -0300

    add.testcases
    Adicionado mais validações de casos de testes para validação de cadastro de colaborador

--- cypress/e2e/CadastroUsuario/crudUsuario.cy.js
+++ cypress/e2e/CadastroUsuario/crudUsuario.cy.js
@@ -10,6 +10,239 @@ import { faker } from '@faker-js/faker';
 
 describe('Validações de Cadastro/Edição/Remoção', () => {   
 
+        it('Cadastro colaborador PIM first name em branco', () => {
+          const randomFullName = faker.person.fullName()
+          cy.log(randomFullName)
+
+          // Acessar site
+          cy.visit('https://opensource-demo.orangehrmlive.com/')
+
+          // Acessar como admin
+          login(credentials.email, credentials.senha)
+
+          // Acessar cadastro de colaboradores
+          cy.xpath(selectors.menuPim.employeeTab).click()
+          cy.get(selectors.menuPim.addButton).click()
+
+          // Cadastrar um colaborador
+          cy.get(selectors.employeeForm.lastName).type('Santos')
+
+          cy.get(selectors.employeeForm.saveButton).click()
+
+          // Verificar sucesso
+          cy.wait(2000)
+          cy.get('.--name-grouped-field > :nth-child(1) > .oxd-text').should('contain', 'Required')
+          cy.screenshot('Não foi possivel cadastrar')
+
+        });
+
+        it('Cadastro colaborador PIM last name em branco', () => {
+          const randomFullName = faker.person.fullName()
+          cy.log(randomFullName)
+
+          // Acessar site
+          cy.visit('https://opensource-demo.orangehrmlive.com/')
+
+          // Acessar como admin
+          login(credentials.email, credentials.senha)
+
+          // Acessar cadastro de colaboradores
+          cy.xpath(selectors.menuPim.employeeTab).click()
+          cy.get(selectors.menuPim.addButton).click()
+
+          // Cadastrar um colaborador
+          cy.get(selectors.employeeForm.firstName).click().type('Trevor')
+
+          cy.get(selectors.employeeForm.saveButton).click()
+
+          // Verificar sucesso
+          cy.wait(2000)
+          cy.get('.--name-grouped-field > :nth-child(3) > .oxd-text').should('contain', 'Required')
+          cy.screenshot('Não foi possivel cadastrar')
+
+        });
+
+        it('Cadastro colaborador PIM first name ultrapassando limite maximo de caracteres', () => {
+          const randomFullName = faker.person.fullName()
+          cy.log(randomFullName)
+
+          // Acessar site
+          cy.visit('https://opensource-demo.orangehrmlive.com/')
+
+          // Acessar como admin
+          login(credentials.email, credentials.senha)
+
+          // Acessar cadastro de colaboradores
+          cy.xpath(selectors.menuPim.employeeTab).click()
+          cy.get(selectors.menuPim.addButton).click()
+
+          // Cadastrar um colaborador
+          cy.get(selectors.employeeForm.firstName).click().type('Trevor teste de mais de 30 caracteres no first name')
+          cy.get(selectors.employeeForm.lastName).type('Santos')
+
+          cy.get(selectors.employeeForm.saveButton).click()
+
+          // Verificar sucesso
+          cy.wait(2000)
+          cy.get('.--name-grouped-field > :nth-child(1) > .oxd-text').should('contain', 'Should not exceed 30 characters')
+          cy.screenshot('Não foi possivel cadastrar')
+
+        });
+
+        it('Cadastro colaborador PIM middle name ultrapassando limite maximo de caracteres', () => {
+          const randomFullName = faker.person.fullName()
+          cy.log(randomFullName)
+
+          // Acessar site
+          cy.visit('https://opensource-demo.orangehrmlive.com/')
+
+          // Acessar como admin
+          login(credentials.email, credentials.senha)
+
+          // Acessar cadastro de colaboradores
+          cy.xpath(selectors.menuPim.employeeTab).click()
+          cy.get(selectors.menuPim.addButton).click()
+
+          // Cadastrar um colaborador
+          cy.get(selectors.employeeForm.firstName).click().type('Trevor')
+          cy.get(':nth-child(2) > :nth-child(2) > .oxd-input').type('teste de mais de 30 caracteres no middle name')
+          cy.get(selectors.employeeForm.lastName).type('Santos')
+
+          cy.get(selectors.employeeForm.saveButton).click()
+
+          // Verificar sucesso
+          cy.wait(2000)
+          cy.get('.--name-grouped-field > :nth-child(2) > .oxd-text').should('contain', 'Should not exceed 30 characters')
+          cy.screenshot('Não foi possivel cadastrar')
+
+        });
+
+        it('Cadastro colaborador PIM last name ultrapassando limite maximo de caracteres', () => {
+          const randomFullName = faker.person.fullName()
+          cy.log(randomFullName)
+
+          // Acessar site
+          cy.visit('https://opensource-demo.orangehrmlive.com/')
+
+          // Acessar como admin
+          login(credentials.email, credentials.senha)
+
+          // Acessar cadastro de colaboradores
+          cy.xpath(selectors.menuPim.employeeTab).click()
+          cy.get(selectors.menuPim.addButton).click()
+
+          // Cadastrar um colaborador
+          cy.get(selectors.employeeForm.firstName).click().type('Trevor')
+          cy.get(selectors.employeeForm.lastName).type('teste de mais de 30 caracteres no last name')
+
+          cy.get(selectors.employeeForm.saveButton).click()
+
+          // Verificar sucesso
+          cy.wait(2000)
+          cy.get('.--name-grouped-field > :nth-child(3) > .oxd-text').should('contain', 'Should not exceed 30 characters')
+          cy.screenshot('Não foi possivel cadastrar')
+
+        });
+
+        it('Cadastro colaborador PIM Username em branco (login details)', () => {
+          const randomFullName = faker.person.fullName()
+          cy.log(randomFullName)
+
+          // Acessar site
+          cy.visit('https://opensource-demo.orangehrmlive.com/')
+
+          // Acessar como admin
+          login(credentials.email, credentials.senha)
+
+          // Acessar cadastro de colaboradores
+          cy.xpath(selectors.menuPim.employeeTab).click()
+          cy.get(selectors.menuPim.addButton).click()
+
+          // Cadastrar um colaborador
+          cy.get(selectors.employeeForm.firstName).click().type('Trevor')
+          cy.get(selectors.employeeForm.lastName).type('Santos')
+
+          // Criação de senha para colaborador
+          cy.get(selectors.employeeForm.createLoginDatails).click()
+
+          preencherSenha1(credentialsColaborador.senha)
+          
+          cy.get(selectors.employeeForm.saveButton).click()
+
+          // Verificar sucesso
+          cy.wait(1000)
+          cy.get('.oxd-input-group > .oxd-text').should('contain', 'Required')
+          cy.screenshot('Não foi possivel cadastrar')
+
+        });
+
+        it('Cadastro colaborador PIM Username não respeitando valor minimo de caracteres (login details)', () => {
+          const randomFullName = faker.person.fullName()
+          cy.log(randomFullName)
+
+          // Acessar site
+          cy.visit('https://opensource-demo.orangehrmlive.com/')
+
+          // Acessar como admin
+          login(credentials.email, credentials.senha)
+
+          // Acessar cadastro de colaboradores
+          cy.xpath(selectors.menuPim.employeeTab).click()
+          cy.get(selectors.menuPim.addButton).click()
+
+          // Cadastrar um colaborador
+          cy.get(selectors.employeeForm.firstName).click().type('Trevor')
+          cy.get(selectors.employeeForm.lastName).type('Santos')
+
+          // Criação de senha para colaborador
+          cy.get(selectors.employeeForm.createLoginDatails).click()
+          cy.get(selectors.employeeForm.usernameField).type('Tre')
+
+          preencherSenha1(credentialsColaborador.senha)
+          
+          cy.get(selectors.employeeForm.saveButton).click()
+
+          // Verificar sucesso
+          cy.wait(1000)
+          cy.get('.oxd-input-group > .oxd-text').should('contain', 'Should be at least 5 characters')
+          cy.screenshot('Não foi possivel cadastrar')
+
+        });
+
+        it('Cadastro colaborador PIM Username não respeitando valor maximo de caracteres (login details)', () => {
+          const randomFullName = faker.person.fullName()
+          cy.log(randomFullName)
+
+          // Acessar site
+          cy.visit('https://opensource-demo.orangehrmlive.com/')
+
+          // Acessar como admin
+          login(credentials.email, credentials.senha)
+
+          // Acessar cadastro de colaboradores
+          cy.xpath(selectors.menuPim.employeeTab).click()
+          cy.get(selectors.menuPim.addButton).click()
+
+          // Cadastrar um colaborador
+          cy.get(selectors.employeeForm.firstName).click().type('Trevor')
+          cy.get(selectors.employeeForm.lastName).type('Santos')
+
+          // Criação de senha para colaborador
+          cy.get(selectors.employeeForm.createLoginDatails).click()
+          cy.get(selectors.employeeForm.usernameField).type('Trevor teste limite de 40 caracteres ultrapassado')
+
+          preencherSenha1(credentialsColaborador.senha)
+          
+          cy.get(selectors.employeeForm.saveButton).click()
+
+          // Verificar sucesso
+          cy.wait(1000)
+          cy.get('.oxd-input-group > .oxd-text').should('contain', 'Should not exceed 40 characters')
+          cy.screenshot('cadastrado_com_sucesso')
+
+
+        });
+
         it('Cadastro colaborador PIM', () => {
           const randomFullName = faker.person.fullName()
           cy.log(randomFullName)
@@ -40,7 +273,6 @@ describe('Validações de Cadastro/Edição/Remoção', () => {
           cy.wait(1000)
           cy.get(selectors.toastMessage).should('contain', 'Success')
           cy.screenshot('cadastrado_com_sucesso')
-
 
         });
   
